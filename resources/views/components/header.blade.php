1182ace73b698c3e5dd89baf7482e198e7155356
Truonghocdot <truonghocdot05@gmail.com>
2026-03-12 02:56:34 +0700

    truonghd - fix

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -139,6 +139,20 @@
             @endauth
         </nav>
     </div>
+    <!-- Header Waves -->
+    <div class="absolute bottom-0 left-0 w-full h-8 overflow-hidden pointer-events-none opacity-40 z-0">
+        <svg class="w-full h-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="header-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="wave-animation">
+                <use xlink:href="#header-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlink:href="#header-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                <use xlink:href="#header-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                <use xlink:href="#header-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </div>
 </header>
 
 <script>
